905886a1d8686f0e560ac3d6429c8d36bceb1bc2
SashaKYotoz <63205134+SashaKYotoz@users.noreply.github.com>
2025-10-14 13:55:28 +0300

    quick fix for item entity

--- src/main/java/net/sashakyotoz/anitexlib/mixin/ItemEntityMixin.java
+++ src/main/java/net/sashakyotoz/anitexlib/mixin/ItemEntityMixin.java
@@ -4,33 +4,32 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.sashakyotoz.anitexlib.client.renderer.IParticleItem;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin {
-    @Unique
-    public ItemEntity aniTexLib$copy = ((ItemEntity) (Object) this);
 
     @Inject(method = "tick", at = @At("RETURN"))
     public void addParticles(CallbackInfo ci) {
-        if (aniTexLib$copy != null) {
-            if (aniTexLib$copy.getItem().getItem() instanceof IParticleItem item) {
-                if (aniTexLib$copy.level().isClientSide())
-                    item.addParticles(aniTexLib$copy.level(), aniTexLib$copy);
-                if (!aniTexLib$copy.level().isClientSide())
-                    item.addParticles(aniTexLib$copy.level(), (ItemEntity) (Object) this);
+        ItemEntity entity = (ItemEntity) ((Object) this);
+        if (entity != null) {
+            if (entity.getItem().getItem() instanceof IParticleItem item) {
+                if (entity.level().isClientSide())
+                    item.addParticles(entity.level(), entity);
+                else
+                    item.serverTick(entity.level(), entity);
             }
         }
     }
 
     @Inject(method = "playerTouch", at = @At("RETURN"))
     public void playerTouch(Player pEntity, CallbackInfo ci) {
-        if (aniTexLib$copy != null) {
-            if (aniTexLib$copy.getItem().getItem() instanceof IParticleItem item)
-                item.onPlayerTouch(pEntity, aniTexLib$copy);
+        ItemEntity entity = (ItemEntity) ((Object) this);
+        if (entity != null) {
+            if (entity.getItem().getItem() instanceof IParticleItem item)
+                item.onPlayerTouch(pEntity, entity);
         }
     }
 }
